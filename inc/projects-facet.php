bcbfba1371188424f7b78147a51058a70a6c3933
woodwardtw <twwoodward@me.com>
2022-11-04 14:40:08 -0400

    project layout stuff

--- inc/projects-facet.php
+++ inc/projects-facet.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || exit;
 
 while ( have_posts() ): the_post(); ?>
     <div class="col-md-3 card-holder">
-            <div class="card position-relative projects-facet">
+            <div class="card position-relative project-facet">
                 <div class="year"><?php conflict_get_year(get_the_id());?></div>
                     <?php echo conflict_resource_image();?>
               <div class="card-body">
